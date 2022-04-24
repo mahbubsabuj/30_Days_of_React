ad5087f0782bd8dcf77fcb42b7191621b4b877ac
mahbubsabuj <mahbubsabuj3@gmail.com>
2022-04-24 15:39:54 +0600

    Day 6

--- Day6/cf-api/src/App.jsx
+++ Day6/cf-api/src/App.jsx
@@ -1,6 +1,7 @@
+import { breakpoints } from "@mui/system";
 import axios from "axios";
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import RankList from "./components/RankList";
 import SearchBar from "./components/SearchBar";
 
@@ -18,6 +19,22 @@ const App = () => {
     console.log(response.data.result.slice(0, 10));
     setRankList(response.data.result.slice(0, 10));
   };
+  const init = async () => {
+    const response = await axios.get(
+      "https://codeforces.com/api/contest.list?gym=false",
+      {}
+    );
+    const results = response.data.result;
+    for (let i = 0; i < results.length; ++i) {
+        if (results[i].phase === "FINISHED") {
+            onTermSubmit(results[i].id);
+            break;
+        }
+    }
+  };
+  useEffect(() => {
+      init();
+  }, []);
   return (
     <div>
       <SearchBar onTermSubmit={onTermSubmit} />
